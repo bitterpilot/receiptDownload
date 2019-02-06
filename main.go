7d283f9d477d1a4f11571b8800327f64f74daf4b
Nathan Minissale <nathanminissale@gmail.com>
2019-02-06 14:34:26 +0800

    Started building get email functions

--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/bitterpilot/receiptDownload/gmail"
 )
 
 type Config struct {
 	Label   string `json:"Label"`
 	Subject string `json:"Subject"`
 	Sender  string `json:"Sender"`
+	SaveLoc string `json:"SaveLoc"`
+
+	//temporary example items
+	Example struct {
+		EmailID string `json:"emailID"`
+	} `json:"example"`
 }
 
 func main() {
-	config := loadConfiguration("./config.json")
-	fmt.Println(config)
+	config := loadConfiguration("config.json")
+	query := fmt.Sprintf("from:%s subject:%s", config.Sender, config.Subject)
+	list := gmail.ListEmails(config.Label, query)
+
+	if len(list) > 3 {
+		//GetEmails
+	} else {
+		for _, val := range list {
+			fmt.Println(val.Id)
+			//GetEmail
+		}
+	}
+	msg := gmail.GetEmail(config.Example.EmailID)
+	partNum := 0
+	for i := 0; i < len(msg.Payload.Parts); i++ {
+		if msg.Payload.Parts[i].MimeType == "text/html" {
+			partNum = i
+		}
+	}
+	data := msg.Payload.Parts[partNum].Body.Data
+	decode, _ := base64.URLEncoding.DecodeString(data)
+	fmt.Printf("%s", decode)
 }
 
 func loadConfiguration(file string) Config {
